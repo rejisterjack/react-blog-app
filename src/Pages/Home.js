c707eac6eb233f87652cc7d488016ce1dd29a932
Rupam Das <83290469+rejisterjack@users.noreply.github.com>
2022-08-31 20:46:40 +0530

    Initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,191 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Card from "../Component/Card";
+import CardHome from "../Component/CardHome";
+import SmallCard from "../Component/SmallCard";
+import { store } from "./Details";
+
+const Home = () => {
+  const [detail, setDetail] = useContext(store);
+  return (
+    <div>
+      <div className="Home">
+        <div className="home__container">
+          <div className="home__first"></div>
+          <div className="home_right_container">
+            <div className="home__sec">
+              <Link to="/fitness">
+                {" "}
+                <img
+                  src="https://images.unsplash.com/photo-1636572481914-a07d36917486?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                  alt="not found"
+                />
+              </Link>
+            </div>
+
+            <div className="home__thir">
+              <Link to="/technology">
+                {" "}
+                <img
+                  src="https://media.istockphoto.com/photos/yellow-helicopter-flying-over-the-rocky-mountains-picture-id1306238094?b=1&k=20&m=1306238094&s=170667a&w=0&h=rPxH62Fy-ojAwwN6xgW6iOaQzfBOSOz8E-vhMPKgPxg="
+                  alt="not found"
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <h1 style={{ marginTop: "40px" }}>The Latest</h1>
+        <hr style={{ width: "200px", thickness: "20px" }} />
+        <div className="home__left left1">
+          {detail
+            .filter((article) => {
+              return article.category === "bollywood";
+            })
+            .map((n) => (
+              <CardHome
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+        <div className="home__left left1">
+          {detail
+            .filter((article) => {
+              return article.category === "technology";
+            })
+            .map((n) => (
+              <CardHome
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+        <div className="home__left left1">
+          {detail
+            .filter((article) => {
+              return article.category === "food";
+            })
+            .map((n) => (
+              <CardHome
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+        <h1 style={{ marginTop: "40px", display: "inline-block" }}>
+          Latest Articles
+        </h1>
+
+        <h1 className="Top">Top Posts</h1>
+        <hr style={{ width: "200px", thickness: "20px" }} />
+
+        <div className="rightbar2">
+          {detail
+            .filter((article) => {
+              return article.category === "mix";
+            })
+            .map((n) => (
+              <Card
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+        <div className="sidebar2">
+          {detail
+            .filter((article) => {
+              return article.category === "mix";
+            })
+            .map((n) => (
+              <SmallCard
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                description={n.description.slice(0, 200)}
+                title={n.title.slice(0, 25)}
+                author={n.author}
+              />
+            ))}
+
+          <div className="advertisement">
+            <p>Advertisement</p>
+          </div>
+        </div>
+        <h1 style={{ marginTop: "40px", display: "inline-block" }}>
+          Latest Stories
+        </h1>
+        <hr />
+
+        <div className="home__left">
+          {detail
+            .filter((article) => {
+              return article.category === "footer1";
+            })
+            .map((n) => (
+              <CardHome
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+
+        <div className="home__left">
+          {detail
+            .filter((article) => {
+              return article.category === "footer2";
+            })
+            .map((n) => (
+              <CardHome
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+
+        <div className="home__left">
+          {detail
+            .filter((article) => {
+              return article.category === "footer3";
+            })
+            .map((n) => (
+              <CardHome
+                key={n.id}
+                articleid={n.id}
+                imgUrl={n.Image}
+                title={n.title}
+                description={n.description.slice(0, 200)}
+                author={n.author}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
